d584556de934a4f0fe77fca75737dcbae34afc2f
Franco Sarubbi <sarubbi.fe@gmail.com>
2026-04-04 21:54:52 -0300

    Initial commit

---
+++ src/test/java/com/iot/managerservice/IotManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iot.managerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotManagerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
